bddd9cd437e8b1680dbc7cb4b5f7bb0cc2519582
Ricardo Magalhães <ricardospilotes@gmail.com>
2022-07-24 01:19:09 -0300

    Add files via upload

--- js/vibrate.js
+++ js/vibrate.js
@@ -1,1 +1,10 @@
-navigator.vibrate(250);
+function startVibrate() {
+  navigator.vibrate(200);
+}
+
+// Stops vibration
+function stopVibrate() {
+  // Clear interval and stop persistent vibrating
+  if (vibrateInterval) clearInterval(vibrateInterval);
+  navigator.vibrate(0);
+}
