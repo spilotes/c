1448aaca74750576bcdfa241785fdcf4ba1cf724
Ricardo Magalhães <ricardospilotes@gmail.com>
2023-01-22 06:26:11 -0300

    Add files via upload

--- js/scrollreveal.js
+++ js/scrollreveal.js
@@ -1,48 +1,8 @@
 ScrollReveal({ reset: true });
 
-ScrollReveal().reveal(".seasons", {
-  duration: 4000,
-  delay: 3000,
-});
-
 ScrollReveal().reveal(".saudacao", {
-  duration: 2000,
-  delay: 1000,
-});
-
-ScrollReveal().reveal(".title", {
   origin: "top",
   distance: "2rem",
   duration: 1500,
   delay: 1800,
 });
-
-ScrollReveal().reveal(".ric", {
-  origin: "top",
-  distance: "2rem",
-  duration: 1500,
-  delay: 1800,
-});
-
-ScrollReveal().reveal(".p", {
-  origin: "top",
-  distance: "2rem",
-  duration: 1500,
-  delay: 1800,
-});
-
-ScrollReveal().reveal(".article", {
-  duration: 4000,
-  delay: 1500,
-});
-
-ScrollReveal().reveal(".carta", {
-  duration: 3000,
-});
-
-ScrollReveal().reveal(".andar-a-pe", {
-  duration: 4000,
-  delay: 1500,
-});
-
-window.navigator.vibrate(1000);
