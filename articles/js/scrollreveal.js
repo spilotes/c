61b192fcd39a3a2de2a15a3d03f194e0b0869e9b
Ricardo Magalhães <ricardospilotes@gmail.com>
2023-01-22 09:30:01 -0300

    Add files via upload

---
+++ articles/js/scrollreveal.js
@@ -0,0 +1,16 @@
+ScrollReveal({ reset: true });
+
+ScrollReveal().reveal(".musica", {
+  duration: 2000,
+  delay: 1000,
+});
+
+ScrollReveal().reveal(".description", {
+  duration: 2000,
+  delay: 1000,
+});
+
+ScrollReveal().reveal("audio", {
+  duration: 2000,
+  delay: 1000,
+});
