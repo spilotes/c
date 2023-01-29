1e9daa9f2807c40d5ccc5de943b00ea10640f827
Ricardo Magalhães <ricardospilotes@gmail.com>
2023-01-29 14:11:31 -0300

    Add files via upload

---
+++ articles/js/scrollreveal.js
@@ -0,0 +1,76 @@
+ScrollReveal({ reset: true });
+
+ScrollReveal().reveal(".title", {
+  duration: 2000,
+  delay: 1000,
+});
+
+ScrollReveal().reveal(".one", {
+  duration: 4000,
+  delay: 1500,
+});
+
+ScrollReveal().reveal(".two", {
+  duration: 4000,
+  delay: 1500,
+});
+
+ScrollReveal().reveal(".tree", {
+  duration: 4000,
+  delay: 1500,
+});
+
+ScrollReveal().reveal(".four", {
+  duration: 4000,
+  delay: 1500,
+});
+
+ScrollReveal().reveal(".five", {
+  duration: 4000,
+  delay: 1500,
+});
+
+ScrollReveal().reveal(".six", {
+  duration: 4000,
+  delay: 1500,
+});
+
+ScrollReveal().reveal(".seven", {
+  duration: 4000,
+  delay: 1500,
+});
+
+ScrollReveal().reveal(".eith", {
+  duration: 4000,
+  delay: 1500,
+});
+
+ScrollReveal().reveal(".nine", {
+  duration: 4000,
+  delay: 1500,
+});
+
+ScrollReveal().reveal(".ten", {
+  duration: 4000,
+  delay: 1500,
+});
+
+ScrollReveal().reveal(".eleven", {
+  duration: 4000,
+  delay: 1500,
+});
+
+ScrollReveal().reveal(".musica", {
+  duration: 2000,
+  delay: 1000,
+});
+
+ScrollReveal().reveal(".description", {
+  duration: 2000,
+  delay: 1000,
+});
+
+ScrollReveal().reveal("audio", {
+  duration: 2000,
+  delay: 1000,
+});
---
+++ letter/js/animation.js
@@ -0,0 +1,31 @@
+ScrollReveal({ reset: true });
+
+ScrollReveal().reveal(".patinho", {
+  duration: 3000,
+});
+
+ScrollReveal().reveal(".talk", {
+  duration: 3000,
+  delay: 1500,
+});
+ScrollReveal().reveal(".span", {
+  duration: 3000,
+  delay: 1500,
+});
+
+ScrollReveal().reveal(".content-coracao", {
+  duration: 2000,
+  delay: 2000,
+});
+
+ScrollReveal().reveal(".text", {
+  duration: 3000,
+});
+
+ScrollReveal().reveal("#audio", {
+  duration: 3000,
+});
+
+ScrollReveal().reveal(".span1", {
+  duration: 3000,
+});
