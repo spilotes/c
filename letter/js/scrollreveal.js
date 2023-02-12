846899c4f48a07e6220eeaf7a965abaf12e36125
Ricardo Magalhães <ricardospilotes@gmail.com>
2023-02-12 02:07:28 -0300

    Add files via upload

---
+++ letter/js/scrollreveal.js
@@ -0,0 +1,33 @@
+ScrollReveal({ reset: true });
+
+ScrollReveal().reveal(".inbox-top", {
+  duration: 4000,
+  delay: 4000,
+});
+
+ScrollReveal().reveal(".patinho", {
+  duration: 3000,
+});
+
+ScrollReveal().reveal(".talk", {
+  duration: 3000,
+  delay: 1500,
+});
+ScrollReveal().reveal(".span", {
+  duration: 3000,
+  delay: 1500,
+});
+
+ScrollReveal().reveal(".content-coracao", {
+  duration: 2000,
+  delay: 2000,
+});
+
+ScrollReveal().reveal(".text", {
+  duration: 3000,
+});
+
+ScrollReveal().reveal(".controls", {
+  duration: 1500,
+  delay: 1800,
+});
