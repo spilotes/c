1d4d91319a0e8443b4d80ed79743c95b0d3fa332
Ricardo Magalhães <ricardospilotes@gmail.com>
2023-07-23 19:33:59 -0300

    Add files via upload

---
+++ letter3/js/read.js
@@ -0,0 +1,23 @@
+function ler1() {
+  window.scrollTo({
+    top: 825,
+    left: 0,
+    behavior: "smooth",
+  });
+}
+
+function ler2() {
+  window.scrollTo({
+    top: 670,
+    left: 0,
+    behavior: "smooth",
+  });
+}
+
+function ler3() {
+  window.scrollTo({
+    top: 798,
+    left: 0,
+    behavior: "smooth",
+  });
+}
