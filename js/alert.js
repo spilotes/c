58a09884db6bebf9d80a460c86543f240450037f
Ricardo Magalhães <ricardospilotes@gmail.com>
2023-08-10 23:27:06 -0300

    Update alert.js

--- js/alert.js
+++ js/alert.js
@@ -6,6 +6,6 @@ function modal() {
       "dialogwidth: 450; dialogheight: 300; resizable: yes"
     );
   } else {
-    alert(" “— Eu sou apenas eu — disse Linus, sem saber para onde a conversa se dirigia. — Não sei ser outra pessoa a não ser eu mesmo. Sempre fui assim. Não é muito, mas faço o melhor que posso com o que tenho. Arthur olhou para ele com tristeza. Estendeu a mão e apertou a de Linus brevemente antes de a soltar. — Suponho que o melhor é tudo o que se pode pedir.” | TJ Klune - A Casa no Mar Cerúleo");
+    alert(" “— <i>Eu sou apenas eu — disse Linus, sem saber para onde a conversa se dirigia. — Não sei ser outra pessoa a não ser eu mesmo. Sempre fui assim. Não é muito, mas faço o melhor que posso com o que tenho. Arthur olhou para ele com tristeza. Estendeu a mão e apertou a de Linus brevemente antes de a soltar. — Suponho que o melhor é tudo o que se pode pedir.”</i> | TJ Klune - A Casa no Mar Cerúleo");
   }
 }
