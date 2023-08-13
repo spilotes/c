0a1c00e279e0b5d44dd2d16c7e1027a2c0e56579
Ricardo Magalhães <ricardospilotes@gmail.com>
2023-08-13 05:17:42 -0300

    Add files via upload

---
+++ telegram/js/scrollreveal.js
@@ -0,0 +1,51 @@
+ScrollReveal({ reset: true });
+
+ScrollReveal().reveal(".hai", {
+  duration: 1000,
+  delay: 1000,
+});
+
+ScrollReveal().reveal(".one", {
+  duration: 1500,
+  delay: 5000,
+});
+
+ScrollReveal().reveal(".two", {
+  duration: 1500,
+  delay: 7500,
+});
+
+ScrollReveal().reveal(".pain-bat", {
+  duration: 1000,
+  delay: 500,
+});
+
+ScrollReveal().reveal(".inbox", {
+  duration: 1000,
+  delay: 4000,
+});
+
+ScrollReveal().reveal(".link-s", {
+  duration: 1000,
+  delay: 10000,
+});
+
+ScrollReveal().reveal(".heen", {
+  duration: 1000,
+  delay: 500,
+});
+
+ScrollReveal().reveal(".balao", {
+  duration: 1000,
+  delay: 1500,
+});
+
+ScrollReveal().reveal(".heen-question", {
+  duration: 1000,
+  delay: 1500,
+});
+
+ScrollReveal().reveal(".ikai-ito", {
+  duration: 1000,
+  delay: 1500,
+});
---
+++ telegram/js/scrollto.js
@@ -0,0 +1,15 @@
+function ler1() {
+  window.scrollTo({
+    top: 1650,
+    left: 0,
+    behavior: "smooth",
+  });
+}
+
+function ler2() {
+  window.scrollTo({
+    top: 1685,
+    left: 0,
+    behavior: "smooth",
+  });
+}
