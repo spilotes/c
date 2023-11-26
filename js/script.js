33af0f80120e8f43f83ffab115f676f4d8f66464
Ricardo Magalhães <ricardospilotes@gmail.com>
2023-11-26 18:37:21 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,10 @@
+const style = `
+    background: transparent;
+    padding: 12px;
+    color: #c9d1d9;
+    font-weight: 500;
+    font-size: 1.3rem;
+    font-style: italic;
+`;
+
+console.log("%cgegelinha, watashi wa mada anata o aishiteimasu...", style);
