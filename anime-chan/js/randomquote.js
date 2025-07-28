db66b4d07ec17bc1d60586c80809f08152faa3dd
Ricardo Magalhães <ricardospilotes@gmail.com>
2025-07-28 13:33:46 -0300

    Update randomquote.js

--- anime-chan/js/randomquote.js
+++ anime-chan/js/randomquote.js
@@ -1,9 +1,9 @@
-const url = "https://api.animechan.io/v1/quotes/random";
+
 const randomQuoteElement = document.getElementById("random-quote");
 const generateQuoteButton = document.getElementById("generate-quote-button");
 
 function generateQuote() {
-  fetch(url)
+  fetch('https://api.animechan.io/v1/quotes/random')
     .then(response => response.json())
     .then(quote => console.log(quote))
     .then((data) => {
